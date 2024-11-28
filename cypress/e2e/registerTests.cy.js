a887d19b2c1de6e534dd22f596c93ab9575e0a2b
Bogdan Rasskazov <b.rasskazov@vkteam.ru>
2024-11-28 17:58:30 +0300

    feat: additional checks for geristration

--- cypress/e2e/registerTests.cy.js
+++ cypress/e2e/registerTests.cy.js
@@ -2,6 +2,7 @@ import RegistrationPage from "../pages/RegistrationPage";
 
 describe("Registration", () => {
   const email = `test${Date.now()}@example.com`;
+  const invalidEmail = `test${Date.now()}`;
 
   it("Успешная регистрация", () => {
     RegistrationPage.visit();
@@ -32,6 +33,36 @@ describe("Registration", () => {
     cy.wait(1500);
 
     cy.get("#ConfirmPassword").type("Password321");
+    cy.wait(1500);
+
+    RegistrationPage.submit();
+    RegistrationPage.checkErrorMessage();
+  });
+
+  it("Регистрация невалидной формой email", () => {
+    RegistrationPage.visit();
+
+    RegistrationPage.fillRegistrationForm({
+      firstName: "Jane",
+      lastName: "Doe",
+      email: invalidEmail,
+      password: "Password123",
+    });
+    cy.wait(1500);
+
+    RegistrationPage.submit();
+    RegistrationPage.checkErrorMessage();
+  });
+
+  it("Регистрация с слишком коротким паролем", () => {
+    RegistrationPage.visit();
+
+    RegistrationPage.fillRegistrationForm({
+      firstName: "Jane",
+      lastName: "Doe",
+      email,
+      password: "a",
+    });
     cy.wait(1500);
 
     RegistrationPage.submit();
